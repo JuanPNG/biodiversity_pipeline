3ef816bdc652dadfa01a33a067c61eecc2313618
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2025-05-08 12:00:58 +0100

    Adding pipeline step to write to BigQuery. Refactoring: docstrings

--- src/taxonomy_pipeline.py
+++ src/taxonomy_pipeline.py
@@ -69,20 +69,41 @@ def taxonomy_pipeline(args, beam_args):
             )
         )
 
+        # (Optional) Export to BigQuery (TO DO)
+        # if args.bq_table:
+        #     (
+        #             validated
+        #             | "WriteToBigQuery" >> beam.io.WriteToBigQuery(
+        #                 table=args.bq_table,
+        #                 schema="utils/bq_taxonomy_schema.json",
+        #                 method="FILE_LOADS",
+        #                 custom_gcs_temp_location=args.temp_location,
+        #                 write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
+        #                 create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED
+        #             )
+        #     )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Fetch and validate species taxonomy")
 
+    # For ES conn
     parser.add_argument("--host", required=True, help="Elasticsearch host URL")
     parser.add_argument("--user", required=True, help="Elasticsearch username")
     parser.add_argument("--password", required=True, help="Elasticsearch password")
     parser.add_argument("--index", required=True, help="Elasticsearch index name")
-
     parser.add_argument("--size", type=int, default=10, help="Page size from ES")
     parser.add_argument("--pages", type=int, default=1, help="Max pages to fetch from ES")
 
+    # ENA throttling
     parser.add_argument("--sleep", type=float, default=0.25, help="Delay (in seconds) between ENA requests")
+
+    # Output file (TO DO: change to GS)
     parser.add_argument("--output", required=True, help="Output path prefix (no extension)")
+
+    # BigQuery options (TO DO)
+    # parser.add_argument("--bq_table", help="BigQuery table (project.dataset.table)")
+    # parser.add_argument("--temp_location", help="GCS temp path for BQ file loads", required=False)
 
     args, beam_args = parser.parse_known_args()
     taxonomy_pipeline(args, beam_args)
