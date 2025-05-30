ed11018e94939a69949bac4fd291323184090dd0
Juan Pablo Narvaez Gomez <juann@ebi.ac.uk>
2025-05-30 12:05:15 +0100

    Refactoring writing helper to accept tuples instead of dicts.

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,3 +1,4 @@
+import json
 import os
 import re
 from apache_beam.io.filesystems import FileSystems
@@ -30,12 +31,12 @@ def write_species_file(kv, output_dir):
     Writes JSONL records for a single species to a file in the output directory.
     `kv`: tuple (species_name, iterable of json strings)
     """
-    species_name, records = kv
+    (species_name, _shard), records = kv
     safe_name = re.sub(r'[^A-Za-z0-9_]', '_', species_name.replace(' ', '_'))
     path = os.path.join(output_dir, f'occ_{safe_name}.jsonl')
     with FileSystems.create(path) as f:
         for line in records:
-            f.write((line + '\n').encode('utf-8'))
+            f.write((json.dumps(line) + "\n").encode("utf-8"))
 
 
 def merge_annotations(inputs):
